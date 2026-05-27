65b9008167b95c923c23d4693168cb7ed1eced13
Pawan Kumar <157190294+Pawan-o3@users.noreply.github.com>
2026-05-27 22:25:56 +0530

    Update database_connect.php

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,10 +1,10 @@
 <?php
 // InfinityFree Database Connection
 
-$DB_HOST = "sql100.infinityfree.com";
-$DB_USER = "if0_40449149";
-$DB_PASS = "password add";   
-$DB_NAME = "if0_40449149_pglife";
+$DB_HOST = "    ";
+$DB_USER = "    ";
+$DB_PASS = "    ";   
+$DB_NAME = "    ";
 
 $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
